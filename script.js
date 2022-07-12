62265b4ee1af1f1c7c778574ded972a4abc384dc
Eddy Cerpa <82135358+edycerpa@users.noreply.github.com>
2022-07-11 21:49:41 -0500

    testing CWD tailwind

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleA(){
+    document.getElementById("toggleA").classList.toggle ("hide");
+    document.getElementById("iconA-toggleA").classList.toggle ("icon-hide");
+    document.getElementById("iconB-toggleA").classList.toggle ("icon-hide");
+}
+function toggleB(){
+    document.getElementById("toggleB").classList.toggle ("hide");
+    document.getElementById("iconA-toggleB").classList.toggle ("icon-hide");
+    document.getElementById("iconB-toggleB").classList.toggle ("icon-hide");
+}
+function toggleC(){
+    document.getElementById("toggleC").classList.toggle ("hide");
+    document.getElementById("iconA-toggleC").classList.toggle ("icon-hide");
+    document.getElementById("iconB-toggleC").classList.toggle ("icon-hide");
+}
