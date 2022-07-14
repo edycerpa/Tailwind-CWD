a66f0e5635f714d51e84d9b7e4d39f18b00012de
Eddy Cerpa <82135358+edycerpa@users.noreply.github.com>
2022-07-13 22:29:00 -0500

    Mejora efecto toggle

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+//let textToggle = 
 function toggleA(){
     document.getElementById("toggleA").classList.toggle ("hide");
+    document.getElementById("toggleA").classList.toggle ("show");
     document.getElementById("iconA-toggleA").classList.toggle ("icon-hide");
     document.getElementById("iconB-toggleA").classList.toggle ("icon-hide");
 }
 function toggleB(){
     document.getElementById("toggleB").classList.toggle ("hide");
+    document.getElementById("toggleB").classList.toggle ("show");
     document.getElementById("iconA-toggleB").classList.toggle ("icon-hide");
     document.getElementById("iconB-toggleB").classList.toggle ("icon-hide");
 }
 function toggleC(){
     document.getElementById("toggleC").classList.toggle ("hide");
+    document.getElementById("toggleC").classList.toggle ("show");
     document.getElementById("iconA-toggleC").classList.toggle ("icon-hide");
     document.getElementById("iconB-toggleC").classList.toggle ("icon-hide");
 } 
